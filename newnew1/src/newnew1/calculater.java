f3beb6a21e92c88aa01e5d8768e218dd965b3bfd
seemal <seemal@DESKTOP-VQE0OQD>
2018-12-02 04:56:57 -0500

    new homework commit push

---
+++ newnew1/src/newnew1/calculater.java
@@ -0,0 +1,16 @@
+package newnew1;
+
+ class calculater {
+	 static int cube(int x) {
+		 return x*x*x;
+		 
+	 }
+
+	public static void main(String[] args) {
+		int result=calculater.cube(5);
+		System.out.print(result);
+		
+
+	}
+
+}
