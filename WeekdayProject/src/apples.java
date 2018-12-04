ad14559196565c254b485098194c6237151e25a4
seemal <schauhan1206@gmail.com>
2018-12-04 16:11:26 -0500

    constructor 1 commit and push

--- WeekdayProject/src/apples.java
+++ WeekdayProject/src/apples.java
@@ -1,15 +1,10 @@
-import java.util.Scanner;
-
 public class apples {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		tuna tunaObject = new tuna ();
+		potpie potObject = new potpie (4,5,6);
 		
-		System.out.println("Enter your name here:");
-		String name = input.nextLine();
 		
-		tunaObject.simpleMessage(name);
+		
 		
 		
 		
