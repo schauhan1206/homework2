8ad93bc6c8d7f104e8dfc12610d28f9c4d941a02
seemal <schauhan1206@gmail.com>
2018-12-04 15:42:01 -0500

    impor scanner commit and push

--- WeekdayProject/src/apples.java
+++ WeekdayProject/src/apples.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class apples {
 
 	public static void main(String[] args) {
-		tuna tunaObject = new tuna();
-		tunaObject.simpleMessage();
+		Scanner input = new Scanner(System.in);
+		tuna tunaObject = new tuna ();
+		
+		System.out.println("Enter your name here:");
+		String name = input.nextLine();
+		
+		tunaObject.simpleMessage(name);
+		
+		
+		
 		
 		
 
--- WeekdayProject/src/tuna.java
+++ WeekdayProject/src/tuna.java
@@ -1,9 +1,13 @@
 
 public class tuna {
-	public void simpleMessage() {
-		System.out.println("This is another class");
+	public void simpleMessage(String name ){
+		System.out.println("Hello"  + name);
+		
+		
+	}
+	
 		
 	}
 	
 
-}
+
